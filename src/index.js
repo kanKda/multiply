118c183177bd300b6ab35540ce3a5e29665f43e3
kanKda <apiecem9@gmail.com>
2019-09-25 17:26:45 +0400

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function multiply(first, second) {
-  // your solution
+	var first = first.split('').reverse();
+	var second = second.split('').reverse();
+
+	var numArr = [];
+
+	for (var i = 0; i < first.length; i++) {
+		for (var j = 0; j < second.length; j++) {
+		var mult = first[i] * second[j];
+			if (numArr[i + j]) {
+			numArr[i + j] += mult;
+			} else {
+			numArr[i + j] = mult;
+			}
+		}
+	}
+
+	for (var i = 0; i < numArr.length; i++) {
+	var num = numArr[i] % 10;
+    var shift = Math.floor(numArr[i] / 10);
+    numArr[i] = num;
+    if (numArr[i + 1])
+      numArr[i + 1] += shift;
+    else if (shift != 0)
+      numArr[i + 1] = shift;
+  	}
+
+
+	return numArr.reverse().join('');
 }
